ed81a067bcbf4bdebf1b9d4036e43ebdc99f9e64
Jaziel Perez <pjaziel723@gmail.com>
2022-03-04 13:39:34 -0600

    temas prueba

---
+++ functions.php
@@ -0,0 +1,74 @@
+<?php
+
+function plz_assets(){
+     
+     wp_register_style("goole-font", "https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700", array(), false, 'all');
+
+     wp_register_style("goole-font-2", "https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap", array(), false, 'all');
+     
+    wp_register_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css", array(), false , 'all');
+    
+
+    wp_enqueue_style("estilos", get_template_directory_uri() . "/assets/css/style.css ", array("goole-font", "bootstrap"));
+
+    wp_enqueue_script("yardsale-js",  get_template_directory_uri() . "/assets/js/script.js");
+
+}
+//  tenemos que ingresar las funciones en la cola del ciclo de vida de worpres  
+
+add_action("wp_enqueue_scripts", "plz_assets");
+
+
+function plz_analytics(){
+   
+   ?>
+    
+   
+   <?php
+}
+
+add_action("wp_body_open", "plz_analytics");
+
+
+
+function plz_theme_supports(){
+   add_theme_support('title-tag');
+   add_theme_support('post-thumbnails');
+   add_theme_support('custom-logo' , 
+     array(
+        "width" => 170, 
+        "height" => 35, 
+        "flex-width" => true,
+        "flex-height" => true,
+     )                             
+               );
+
+}
+
+add_action("after_setup_theme","plz_theme_supports");
+
+
+function plz_add_menus(){
+   register_nav_menus (
+        array( 
+         'menu-principal' => "Menu Principal", 
+         'menu-responsive' => "Menu responsive",
+         ) 
+
+   );
+
+
+} 
+
+add_action("after_setup_theme", "plz_add_menus");
+
+function plz_add_sidebar(){
+    register_sidebar(
+       array(
+          "name" => "Pie de pagina",
+          "id" => 'pie-pagina',
+          
+       )
+    );
+
+}
